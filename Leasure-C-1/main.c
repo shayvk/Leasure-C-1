bfcc2e1a293ed585eefd82992ebd2d0b5f03c393
Shay Vaknin <shayv@pm.me>
2019-07-12 22:20:22 +0300

    Initial Commit

---
+++ Leasure-C-1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leasure-C-1
+//
+//  Created by Shay Vaknin on 12/07/2019.
+//  Copyright © 2019 Shay Vaknin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!");
+    return 0;
+}
